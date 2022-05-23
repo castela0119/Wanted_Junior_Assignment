1f33be0ad4e63bd7a37ed15be3e2462e5d2f5c9f
castela0119 <castela0119@gmail.com>
2022-05-23 17:07:41 +0900

    feat : uploaders.py 작성

---
+++ uploaders.py
@@ -0,0 +1,35 @@
+import os
+import csv
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings") 
+django.setup()
+
+from apps.auto.models import Company
+
+
+CSV_PATH = './wanted_temp_data.csv'
+
+def insert_client():
+
+    with open(CSV_PATH, newline='') as csvfile:
+        rows = csv.DictReader(csvfile)
+
+        company_list = []
+        
+        for row in rows:
+            company_list.append(Company(
+                company_ko = row['company_ko'],
+                company_en = row['company_en'],
+                company_ja = row['company_ja'],
+                tag_ko = row['tag_ko'],
+                tag_en = row['tag_en'],
+                tag_ja = row['tag_ja'],
+            ))
+
+        Company.objects.bulk_create(company_list)
+        
+        print('COMPANY DATA UPLOADED SUCCESSFULY!')
+        
+
+insert_client()
