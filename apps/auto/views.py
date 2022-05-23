348886a3e37e0594adbcae2ce7a3ccc7d73ef556
castela0119 <castela0119@gmail.com>
2022-05-23 17:31:05 +0900

    fix : SearchAPI 수정

--- apps/auto/views.py
+++ apps/auto/views.py
@@ -12,31 +12,31 @@ class AutoSearchAPI(APIView):
         """
         word = request.GET.get('word', '')
 
-        data = {}
+        data = []
 
-        ko_name       = Company.objects.filter(korean_name__icontains = word).exists()
-        en_name       = Company.objects.filter(english_name__icontains = word).exists()
-        category_name = Company.objects.filter(name__icontains = word).exists()
+        ko_name       = Company.objects.filter(company_ko__icontains = word).exists()
+        en_name       = Company.objects.filter(company_en__icontains = word).exists()
+        ja_name       = Company.objects.filter(company_ja__icontains = word).exists()
 
         if ko_name:
-                objs  = Company.objects.filter(korean_name__icontains = word)
+                objs  = Company.objects.filter(company_ko__icontains = word)
                 for obj in objs:
                     data.append({
                         'word' : obj.company_ko
                     })
 
         if en_name:
-            objs  = Company.objects.filter(english_name__icontains = word)
+            objs  = Company.objects.filter(company_en__icontains = word)
             for obj in objs:
                 data.append({
                     'word' : obj.company_en
                 })
 
-        if category_name:
-            objs = Company.objects.filter(name__icontains = word)
+        if ja_name:
+            objs = Company.objects.filter(company_ja__icontains = word)
             for obj in objs:
                 data.append({
-                    'word' : obj.company_jn
+                    'word' : obj.company_ja
                 })
         
         return Response(data)
